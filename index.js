63c3101e3821c046eaebec396a80d1254f766afc
Alfredo Morales Pinzon <amoralespinzon@bwh.harvard.edu>
2019-04-22 23:30:01 -0400

    Tool example was validated.

--- index.js
+++ index.js
@@ -74,6 +74,11 @@ var main = async () => {
     method: 'GET'
   };
 
+  let optionsTool = {
+    url: baseSchema_Url + 'tool.schema.json',
+    method: 'GET'
+  };
+
   var workflowSchemaString = await sendRequest(optionsWorkflow)
   var workflowSchema = JSON.parse(workflowSchemaString)
 
@@ -83,8 +88,13 @@ var main = async () => {
   var roiSchemaString = await sendRequest(optionsRoi)
   var roiSchema = JSON.parse(roiSchemaString)
 
+  console.log("coreSchema.defintions.roiInOut:", coreSchema.definitions.roiInOut)
+
   var annotationSchemaString = await sendRequest(optionsAnnotation)
   var annotationSchema = JSON.parse(annotationSchemaString)
+
+  var toolSchemaString = await sendRequest(optionsTool)
+  var toolSchema = JSON.parse(toolSchemaString)
 
   // Validate schemas
 
@@ -93,7 +103,7 @@ var main = async () => {
   	console.log("Validate schemas:")
 
     var ajvOptions = {
-	    schemas: [workflowSchema, coreSchema, roiSchema, annotationSchema],
+	    schemas: [workflowSchema, coreSchema, roiSchema, annotationSchema, toolSchema],
 	    allErrors: true
 	  }
 
@@ -113,6 +123,11 @@ var main = async () => {
 
 	  if(validateRoi)
 	  	console.log("validateRoi OK")
+
+    var validateTool = ajv_url.getSchema('https://raw.githubusercontent.com/SPINEProject/SPINE-json-schema/master/schemas/tool.schema.json');
+
+    if(validateTool)
+      console.log("validateTool OK")
 
 	  console.log("")
 
@@ -134,11 +149,139 @@ var main = async () => {
     method: 'GET'
   };
 
+  let optionsToolExample = {
+    url: baseInstance_Url + 'tool_annotationThreeViewers.json',
+    method: 'GET'
+  };
+
   // Validate schema instances
   console.log("Validate instances:")
 
   var roiInstance = JSON.parse(await sendRequest(optionsRoiExample))
   var annotationInstance = JSON.parse(await sendRequest(optionsAnnotationExample))
+  var toolInstance = JSON.parse(await sendRequest(optionsToolExample))
+
+  var tool = {
+    "name": "Lesion annotation with three viewers",
+    "description": "This annotation configuration allows the annotation implicit ROIs using points",
+    "version": "1.0.2",
+    "owner": "user@email.com",
+    "privacy": "PUBLIC",
+    "creationDate": "April 22, 2019",
+    "type": "ANNOTATION",
+    
+    "annotationTables": {
+      "lesion": {
+        "defaultAnnotations": [
+          {
+            "typeAnnotation": "text",
+            "annotationProperties": {
+              "ontologyId":"https://bioportal.bioontology.org/ontologies/SNOMEDCT",
+              "ontologyValue": "http://purl.bioontology.org/ontology/SNOMEDCT/52988006",
+              "PreferredName" : "Lesion"
+            }
+          }
+        ],
+        "userAnnotations": [
+          {
+            "title": "Location",
+            "typeAnnotation": "text",
+            "ontologyId": "https://bioportal.bioontology.org/ontologies/FMA",
+            "hasDefaultValue": false,
+            "possibleValues": [
+              {
+                "valueInOntology": "http://purl.org/sig/ont/fma/fma258716",
+                "valueToDisplay" : "Left thalamus"
+              },
+              {
+                "valueInOntology": "http://purl.org/sig/ont/fma/fma258714",
+                "valueToDisplay" : "Right thalamus"
+              },
+              {
+                "valueInOntology": "http://purl.org/sig/ont/fma/fma7647",
+                "valueToDisplay" : "Spinal cord"
+              }
+            ]
+          } 
+        ]
+      }
+    },
+
+    "inputs": {
+      "imagesA":{
+        "name": "Input images",
+        "description": "Input images for the annotation tool",
+        "isList": true,
+        "filter": {
+          "test": "test"
+        },
+        "type": "imageEntityInOut",
+        "imageEntityInOut_FileFormat": "dicom.gz",
+        "imageEntityInOut_Type": "ANATOMICAL",
+        "required": true
+      }
+    },
+
+    "outputs": {
+      "rois":{
+        "name": "Created ROIs",
+        "description": "ROIs created with the annotation tool",
+        "isList": true,
+        "type": "roiInOut",
+        "roiInOut_FileFormat": "json"
+      }
+    },
+
+    "configuration":{
+      "viewers": {
+        "generalConfiguration":{
+          "linking":{
+            "linkingControl": true,
+            "linkingInitialValue": false
+          }
+        },
+        "specificConfiguration":{
+          "left":{
+            "name": "left viewer",
+            "location": {
+              "row": 1,
+              "column": 1,
+              "rowspan": 1,
+              "columnspan": 1
+            },
+            "windowLevel":{
+              "userCanChangeWindowLevel": true
+            },
+            "displayImages":{
+              "possibleImagesToDisplay": ["imagesA"]
+            },
+            "hasDefaultImageToDisplay": false,
+            "displayControls":{
+              "orientations": ["axial", "sagittal", "coronal"],
+              "defaultOrientation": "axial",
+              "smoothing": {
+                "smoothingControl": true,
+                "smoothingDefault": false
+              }
+            }
+          }
+        }
+      },
+
+      "widgets":{
+        "pointSelector":{
+          "roiAnnotationLinks": [
+            {
+              "nameRoi": "lesion",
+              "relationWithRoi": "implicit",
+              "implicitRelationWithRoi":"inside",
+              "geometryPointer": "point"
+            }
+          ]
+        }
+      }
+    }
+  }
 
   var validAnnotation = validateAnnotation(annotationInstance)
   if (!validAnnotation){
@@ -150,12 +293,20 @@ var main = async () => {
 
   var validRoi = validateRoi(roiInstance)
   if (!validRoi){
-  	console.log("Not valid roi. Error:", validRoi.errors);
+  	console.log("Not valid roi. Error:", validateRoi.errors);
   }
   else{
   	console.log("Roi instance OK")
   }
 
+  var validTool = validateTool(toolInstance)
+  if (!validTool){
+    console.log("Not valid tool. Error:", validateTool.errors);
+    console.log("Not valid tool. Error[0]:", validateTool.errors[0]);
+  }
+  else{
+    console.log("Tool instance OK")
+  }
 
 }
 
